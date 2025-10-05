494bdb4246051794471901bcb9f50fc02348f879
NaveenGEHU <naveensinghnegiwork@gmail.com>
2025-10-05 08:55:54 +0530

    Add files via upload

---
+++ frontend/app.js
@@ -0,0 +1,49 @@
+function openPage(url)
+{
+    window.location.href = url;
+}
+
+
+
+
+
+function student_validate_login()
+{
+
+}
+function student_register()
+{
+
+}
+function teacher_register()
+{
+
+}
+function teacher_validate_login()
+{
+
+}
+function createtest()
+{
+
+}
+function addquestion()
+{
+
+}
+function loadtest()
+{
+
+}
+function loadresult()
+{
+
+}
+function sortbysection()
+{
+sortbymarks();
+}
+function sortbymarks()
+{
+
+}
